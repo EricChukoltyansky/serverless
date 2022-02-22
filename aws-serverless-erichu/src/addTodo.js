1206cbf8df068bf01d2fed916c0a4f007aacfa81
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-22 12:42:37 +0200

    fix fetchTodos and create fetchTodo function

--- aws-serverless-erichu/src/addTodo.js
+++ aws-serverless-erichu/src/addTodo.js
@@ -4,23 +4,32 @@ const { v4 } = require("uuid");
 const AWS = require("aws-sdk");
 
 const addTodo = async (event) => {
-  const dynamoDB = AWS.DynamoDB.DocumentClient();
+
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
   const { todo } = JSON.parse(event.body);
   const createdAt = new Date();
   const id = v4();
 
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    completed: false
+
+  }
+
   await dynamoDB.put({
     TableName: "TodoTable",
     Item: newTodo,
-  });
+  }).promise();
 
   return {
     statusCode: 200,
-    body: JSON.stringify,
+    body: JSON.stringify(newTodo),
   };
 };
 
 module.exports = {
-  handler: hello,
+  handler: addTodo,
 };
